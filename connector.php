3a6fd988c69948ef4d511c5a92c62aaebde37a4f
Innovationv <web.dev@innovationv.com>
2016-09-19 19:47:37 -0400

    initiation of connector loads db credentials from environment

--- connector.php
+++ connector.php
@@ -1,13 +1,31 @@
 <?php
+// pull in values from environment variables
+$db_name = getenv('DB_NAME');
+$db_username = getenv('DB_USERNAME');
+$db_password = getenv('DB_PASSWORD');
+
+//instantiate the connector
+$connector = new connector();
 
 class connector {
+  private $db;
+
   function __construct(){
+    $db = new db_connection($db_name, $db_username, $db_password);
     //TODO initialize the database connection here
   }
 }
 
 //TODO database connector class
 class db_connection {
-  function __construct(){
+
+  private $db_name;
+  private $db_username;
+  private $db_password;
+
+  function __construct($db_name, $db_username, $db_password){
+    echo $this->$db_name = $db_name;
+    echo $this->$db_username = $db_username;
+    echo $this->$db_password = $db_password;
   }
 }
