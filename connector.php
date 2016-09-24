9f46b59c3f089a0f778bb0ac43042b64fd66c696
Innovationv <web.dev@innovationv.com>
2016-09-24 17:11:55 -0400

    connector creates array of users with their subs when instantiated

--- connector.php
+++ connector.php
@@ -1,7 +1,7 @@
 <?php
 // pull in values from environment variables
 
-require_once "spyc/Spyc.php";
+require_once 'spyc/Spyc.php';
 $db_data = Spyc::YAMLLoad('db.yaml');
 
 echo var_dump($db_data);
@@ -10,28 +10,70 @@ $db_name = $db_data['db_name'];
 $db_username = $db_data['db_username'];
 $db_password = $db_data['db_password'];
 
-//instantiate the connector
-$connector = new connector();
+//instantiate the Connector
+$connector = new Connector();
 
-class connector {
-  private $db;
+class Connector {
+  public $users = [];
 
   function __construct(){
-    $db = new db_connection($db_name, $db_username, $db_password);
-    //TODO initialize the database connection here
+    $conn = new mysqli($db_name, $db_username, $db_password);
+    if ($conn->connect_error) {
+          die('Connection failed: ' . $conn->connect_error);
+    }
+    $this->getUsers($conn);
+    foreach($this->users as $user){
+      $user->updateSubscriptionData($conn);
+    }
+    $conn->close();
+  }
+
+  function getUsers($conn){
+    //clear any previous users
+    unset($this->users);
+    $this->users = [];
+
+    $sql = 'SELECT * from cs3wv_usermeta WHERE meta_key = "ms_username"';
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        //TODO for each row create a new user and push it to the array
+        echo 'CREATING USER INSTANCE: name: ' . $row['meta_value'];
+        $this->createNewUser($row);
+      }
+    } else {
+      echo '0 results';
+    }
+  }
+
+  function createNewUser($row){
+    $users = VestaUser($row);
   }
 }
 
-//TODO database connector class
-class db_connection {
+class VestaUser {
+  public $userId;
+  public $subscriptions = "";
+  public $userName;
 
-  private $db_name;
-  private $db_username;
-  private $db_password;
+  function __construct($row){
+    $this->userId = $row('user_id');
+    $this->userName = $row('meta_value');
+  }
 
-  function __construct($db_name, $db_username, $db_password){
-    echo $this->$db_name = $db_name;
-    echo $this->$db_username = $db_username;
-    echo $this->$db_password = $db_password;
+  public function updateSubscriptionData($conn){
+    $sql = 'SELECT * from cs3wv_usermeta WHERE meta_key = "ms_subscriptions" AND WHERE user_id = ' . $this->userId ;
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo 'UPDATING SUBS DATA: name: ' . $this->userName;
+        $this->subscriptions = ($row['meta_value']);
+      }
+    } else {
+      echo '0 results';
+    }
   }
 }
+
